cb0ceb2a0b3169b6371b05ad1223ff6b9d65c278
Meir Blachman <meirblachman@gmail.com>
2023-11-12 08:26:48 +0200

    using nuget client

--- NupkgDownloader.WebApp/Controllers/NupkgDownloaderController.cs
+++ NupkgDownloader.WebApp/Controllers/NupkgDownloaderController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Routing;
+using NuGet.Packaging.Core;
+using NuGet.Versioning;
+using NupkgDownloader.Core;
 
 namespace NupkgDownloader.WebApp.Controllers;
 
@@ -8,20 +10,35 @@ namespace NupkgDownloader.WebApp.Controllers;
 public class NupkgDownloaderController : ControllerBase
 {
     private readonly ILogger<NupkgDownloaderController> _logger;
+    private readonly NupkgFetcher _nupkgFetcher;
 
-    public NupkgDownloaderController(ILogger<NupkgDownloaderController> logger)
+    public NupkgDownloaderController(NupkgFetcher nupkgFetcher, ILogger<NupkgDownloaderController> logger)
     {
+        _nupkgFetcher = nupkgFetcher;
         _logger = logger;
     }
 
-    [HttpGet(template: "get-download-links", Name = "DisplayDownloadLinks")]
-    public IActionResult GetDownloadLinks(string packageId, string packageVersion)
+    [HttpGet(template: "get-download-links")]
+    public async Task<IActionResult> DisplayDownloadLinks(string packageId, string packageVersion)
     {
-        return Ok();
+        _logger.LogInformation("GetDownloadLinks called with packageId: {packageId} and packageVersion: {packageVersion}", packageId, packageVersion);
+        var package = new PackageIdentity(packageId, NuGetVersion.Parse(packageVersion));
+        var packages = await _nupkgFetcher.GetPackagesDependenciesAsync(package, SourceRepositoryCreator.NugetGallery);
+
+        return Ok(packages.Select(x => new
+        {
+            id = x.PackageIdentity.Id,
+            version = x.PackageIdentity.Version.ToNormalizedString(),
+            downloadLink = Url.Link(nameof(DisplayDownloadLink), new
+            {
+                packageId = x.PackageIdentity.Id,
+                packageVersion = x.PackageIdentity.Version.ToNormalizedString()
+            })
+        }));
     }
 
-    [HttpGet(template: "get-download-link", Name = "DisplayDownloadLink")]
-    public IActionResult GetDownloadLink(string packageId, string packageVersion)
+    [HttpGet(template: "get-download-link", Name = nameof(DisplayDownloadLink))]
+    public IActionResult DisplayDownloadLink(string packageId, string packageVersion)
     {
         return Redirect($"https://www.nuget.org/api/v2/package/{packageId}/{packageVersion}");
     }
--- NupkgDownloader.WebApp/Program.cs
+++ NupkgDownloader.WebApp/Program.cs
@@ -1,8 +1,11 @@
+using NupkgDownloader.Core;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddNupkgFetcher();
 
 var app = builder.Build();
 
